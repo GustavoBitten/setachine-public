53524482fe39ca1b4ec428959864b3b25f7913df
Gustavo Duarte Bittencourt <60633383+GustavoBitten@users.noreply.github.com>
2025-01-16 18:04:53 -0300

    Update first_run.ps1

--- first_run.ps1
+++ first_run.ps1
@@ -1,6 +1,24 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
+scoop install git
 scoop bucket add extras
+scoop install main/gh
 scoop install extras/googlechrome
+scoop install extras/github
+scoop install extras/dbeaver
+scoop install extras/lens
+scoop install extras/vscode
+scoop install extras/teamviewer
 
+
+
+
+# log in chrome 
+#
+
+gh auth login -p https -h github.com -w
+
+New-Item -ItemType Directory prj-personal
+
+gh repo clone GustavoBitten/setachine-public prj-personal/setachine-public
