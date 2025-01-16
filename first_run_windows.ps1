6187ff3772cea0a786a8eaed8eff6ada92a123b9
Gustavo Duarte Bittencourt <gustavoduarte1999@hotmail.com>
2025-01-16 18:12:42 -0300

    chore(docs): add README for project setup on Windows and Linux

--- first_run_windows.ps1
+++ first_run_windows.ps1
@@ -1,7 +1,5 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
-scoop install git
+
 scoop bucket add extras
 scoop install main/gh
 scoop install extras/googlechrome
@@ -13,12 +11,8 @@ scoop install extras/teamviewer
 
 
 
-
 # log in chrome 
 #
 
 gh auth login -p https -h github.com -w
 
-New-Item -ItemType Directory prj-personal
-
-gh repo clone GustavoBitten/setachine-public prj-personal/setachine-public
